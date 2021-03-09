d030ec0b6b5e8b25139e24a579b6225277447097
Dartis4 <daneartis4@gmail.com>
2021-03-08 20:29:43 -0800

    Test scene to work with light

--- js/index.js
+++ js/index.js
@@ -7,12 +7,15 @@ animate();
 function init() {
     // Create the main scene for the 3D drawing
     scene = new THREE.Scene();
+    // scene.background = new THREE.Color("white");
 
     // Every scene needs a camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
     // the renderer renders the scene using the objects, lights and camera
     renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     // Attach the threeJS renderer to the HTML page
@@ -25,14 +28,58 @@ function init() {
         camera.updateProjectMatrix();
     });
 
+    camera.position.x = 40;
+    camera.position.y = 40;
     camera.position.z = 100;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    const skyColor = 0xffffff; // light blue
+    const groundColor = 0xc2805f; // brownish orange
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(skyColor, intensity);
+    light.castShadow = true;
+    light.position.set(0, 15, 0);
+    light.target.position.set(-4, 0, -4);
+    scene.add(light);
+    scene.add(light.target);
+
+    const planeSize = 10000;
+
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({ color: 0xc2805f, side: THREE.DoubleSide });
+    const plane = new THREE.Mesh(planeGeo, planeMat);
+    plane.receiveShadow = true;
+    plane.rotation.x = Math.PI * -0.5;
+
+    scene.add(plane);
+
+    boxSize = 20;
+    boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.position.set(0, 12, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    scene.add(box);
+
+    sphereGeometry = new THREE.SphereGeometry(12, 32, 32);
+    sphereMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff });
+    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(50, 12, 0);
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+    scene.add(sphere);
 }
 
 // This is the game/animation loop
 // This is called ~60 times a second
 function animate() {
     requestAnimationFrame(animate);
+
+    let time = new Date().getTime() * 0.001;
+
+    sphere.position.x = Math.sin(time) * 50;
+    sphere.position.z = Math.cos(time) * 50;
 
     renderer.render(scene, camera);
 }
