f6b9c91da60847c79b32abae884bda019a9d123a
Dartis4 <daneartis4@gmail.com>
2021-03-08 18:00:09 -0800

    Initializing the files

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var scene, camera, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+    // Create the main scene for the 3D drawing
+    scene = new THREE.Scene();
+
+    // Every scene needs a camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+
+    // the renderer renders the scene using the objects, lights and camera
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Attach the threeJS renderer to the HTML page
+    document.body.appendChild(renderer.domElement);
+
+    // window.addEventListener("resize", () => {
+    //     renderer.setSize(window.innerWidth, window.innerHeight);
+    //     camera.aspect = window.innerWidth / window.innerHeight;
+
+    //     camera.updateProjectMatrix();
+    // });
+
+    camera.position.x = 40;
+    camera.position.y = 40;
+    camera.position.z = 100;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+// This is the game/animation loop
+// This is called ~60 times a second
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
