724c7db01b89be66c6365d4e76bf3d0a54b91949
AzJCx&$~#!yb0%aJwSP <Daneartis4@gmail.com>
2021-03-15 15:41:49 -0700

    Fixed three.js and module imports; Simplified file structure

--- index.js
+++ index.js
@@ -1,66 +1,43 @@
+import * as THREE from "./three/build/three.module.js";
+import { OrbitControls } from "./three/examples/jsm/controls/OrbitControls.js";
+
 var scene, camera, renderer;
 var geometry, material, mesh;
 
 init();
 animate();
 
-import { OrbitControls } from './.resources/node_modules/three/examples/js/controls/OrbitControls'
-
 // ctrl + shift + i in the browser brings up developer tools & shows error messages
 
 function init() {
-    class Box {
-        constructor(boxSize, boxColor) {
-            var boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
-            var boxMaterial = new THREE.MeshPhongMaterial({ color: boxColor });
-            var box = new THREE.Mesh(boxGeometry, boxMaterial);
-            box.position.y += boxSize / 2;
-            scene.add(box);
-        }
-    }
-
-    /*******************************************************************************************
-     * This section contains the bare necessities for THREE.js, like the scene, renderer, camera,
-     * etc.
-     ******************************************************************************************/
     // Create the main scene for the 3D drawing
     scene = new THREE.Scene();
-    
 
     // Every scene needs a camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
-
     // the renderer renders the scene using the objects, lights and camera
     renderer = new THREE.WebGLRenderer();
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
     renderer.setSize(window.innerWidth, window.innerHeight);
-
 
     // Attach the threeJS renderer to the HTML page
     document.body.appendChild(renderer.domElement);
 
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     window.addEventListener("resize", () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
         camera.aspect = window.innerWidth / window.innerHeight;
-        // index.js:28 Uncaught TypeError: camera.updateProjectMatrix is not a function
-        // camera.updateProjectMatrix();
+        camera.updateProjectionMatrix();
     });
 
-    camera.position.x = 1;
-    camera.position.y = 1;
-    camera.position.z = 10;
+    camera.position.x = 40;
+    camera.position.y = 40;
+    camera.position.z = 100;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-
 
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.update();
-
-
-
 
     /*******************************************************************************************
      * This section changes the lighting and background
@@ -68,7 +45,6 @@ function init() {
     // Change these to change the ground and sky color
     const skyColor = 0xffffff; // light blue
     const groundColor = 0x07e31d; // lawn green
-
 
     const intensity = 1;
     const light = new THREE.DirectionalLight(skyColor, intensity);
@@ -88,36 +64,26 @@ function init() {
     const plane = new THREE.Mesh(planeGeo, planeMat);
     plane.receiveShadow = true;
     plane.rotation.x = Math.PI * -0.5;
-
     scene.add(plane);
 
-    // boxSize = 20;
-    // boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
-    // boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-    // box = new THREE.Mesh(boxGeometry, boxMaterial);
-    // box.position.set(0, 12, 0);
-    // box.castShadow = true;
-    // box.receiveShadow = true;
-    // scene.add(box);
-    box = new Box(20, 0xff0000);
-
-    sphereGeometry = new THREE.SphereGeometry(12, 32, 32);
-    sphereMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff });
-    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.set(50, 12, 0);
-    sphere.castShadow = true;
-    sphere.receiveShadow = true;
-    scene.add(sphere);
+    const boxSize = 20;
+    const boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    const box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.position.set(0, 12, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    scene.add(box);
 
     // Load in the Table
-    const objectLoader = new THREE.ObjectLoader();
-    objectLoader.load("../blender/table.obj", (root) => {
-        root.position.set(0, 0, 0);
-        scene.add(root);
-    });
+    // const objectLoader = new THREE.ObjectLoader();
+    // objectLoader.load("../../.resources/blender/table.obj", (root) => {
+    //     root.position.set(0, 0, 0);
+    //     scene.add(root);
+    // });
 
-    var loader = new THREE.FontLoader();
-    loader.load("../three.js-master/examples/fonts/helvetiker_regular.typeface.json", function (font) {
+    const fontLoader = new THREE.FontLoader();
+    fontLoader.load("three/examples/fonts/helvetiker_regular.typeface.json", function (font) {
         var textGeometry = new THREE.TextGeometry("X O", {
             font: font,
 
@@ -139,6 +105,7 @@ function init() {
 
         scene.add(mesh);
     });
+    controls.update();
 }
 
 // This is the game/animation loop
@@ -148,9 +115,6 @@ function animate() {
 
     let time = new Date().getTime() * 0.001;
 
-    // sphere.position.x = Math.sin(time) * 50;
-    // sphere.position.z = Math.cos(time) * 50;
-    
     // This updates orbit controls every frame
     controls.update();
 
